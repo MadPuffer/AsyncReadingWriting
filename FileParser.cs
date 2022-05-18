444ba4c028e614a0a3c934a6ea5bfab3fac600a5
Bulat <57670154+MadPuffer@users.noreply.github.com>
2022-05-18 14:55:28 +0300

    Add files via upload

---
+++ FileParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace Parser
+{
+    public static class FileParser
+    {
+        public static List<string> Parse(string path)
+        {
+            var records = new List<string>();
+            using (var reader = File.OpenText(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var record = reader.ReadLine();
+                    if (record.Split(';')[0].StartsWith("Play"))
+                    {
+                        var splittedRecord = record.Split(';');
+                        var channel = splittedRecord[1].Split('=')[1];
+                        var date = DateTime.Parse(splittedRecord[2].Split('=')[1]);
+                        int duration = (int) (DateTime.Parse(splittedRecord[3]
+                            .Split('=')[1]) - date).TotalSeconds;
+
+                        records.Add($"{channel}, {date}, {duration}");
+                    }
+                }
+                reader.Close();
+            }
+            return records;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Parser
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string sourceDirName = Console.ReadLine();
+            string destinationFileName = Console.ReadLine();
+            string[] files = Directory.GetFiles(sourceDirName);
+            List<List<string>> parsedFiles = new List<List<string>>();
+
+            foreach(var i in files)
+            {
+                parsedFiles.Add(FileParser.Parse(i));
+            }
+
+            File.Create(destinationFileName);
+            {
+                foreach (var records in parsedFiles)
+                {
+                    File.WriteAllLines(destinationFileName ,records);
+                }
+            }
+
+        }
+    }
+}
